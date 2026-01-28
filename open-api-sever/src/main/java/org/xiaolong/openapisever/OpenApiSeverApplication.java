f57f9f6ac7b37e511a30de43253d54b3af61f48c
xiaolongsya <1740249376@qq.com>
2026-01-28 23:37:02 +0800

    构建spirngboot基本框架和引入基本需要的依赖

---
+++ open-api-sever/src/main/java/org/xiaolong/openapisever/OpenApiSeverApplication.java
@@ -0,0 +1,15 @@
+package org.xiaolong.openapisever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiSeverApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OpenApiSeverApplication.class, args);
+    }
+
+}
---
+++ open-api-sever/src/test/java/org/xiaolong/openapisever/OpenApiSeverApplicationTests.java
@@ -0,0 +1,15 @@
+package org.xiaolong.openapisever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiSeverApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
